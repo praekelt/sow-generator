e2b787051174c2f849336c87f8705424a1110b2d
hedleyroos <hedleyroos@gmail.com>
2014-10-16 09:27:07 +0200

    Initial skeleton

---
+++ setup.py
@@ -0,0 +1,34 @@
+import multiprocessing
+from setuptools import setup, find_packages
+
+setup(
+    name='sow-generator',
+    version='0.1',
+    description='Create a scope of work from templates and version controlled documentation.',
+    long_description = open('README.rst', 'r').read() + open('CHANGELOG.rst', 'r').read() + open('AUTHORS.rst', 'r').read(),
+    author='Hedley Roos',
+    author_email='hedley@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/sow-generator',
+    packages = find_packages(),
+    install_requires = [
+        'Django',
+        'celery',
+        'django-celery',
+    ],
+    include_package_data=True,
+    tests_require=[
+        'django-setuptest>=0.1.4',
+    ],
+    test_suite="setuptest.setuptest.SetupTestSuite",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
